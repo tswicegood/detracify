a33af31bcd410242e37c7bf4035167aeb03cc6a6
Steve Ivy <steve@wallrazer.com>
2011-09-09 13:09:48 -0700

    refactor a bit on the githubapi object, change the management command to use correctly

--- detracify/website/githubapi.py
+++ detracify/website/githubapi.py
@@ -8,28 +8,18 @@ API_BASE = getattr(settings, 'GITHUB_API_BASE', 'http://github.com/api/v2/json/p
 import logging
 log = logging.getLogger('detracify.github')
 
-def get_open_pull_requests():
-    open_reqs_url = API_BASE + "pulls/django/django/open"
-    log.debug('fetching: %s' % open_reqs_url)
-    r = requests.get(open_reqs_url)
-    log.debug(r.status_code)
-    if not r.status_code == 200:
-        log.error('Github returned a non-200 response!: %s' % r.content)
-#    log.debug(r.content)
-    data = json.loads(r.content)
-    pulls = data['pulls']
-    for pull in pulls:
-        log.info("""
-Pull %s:
-        number: %d
-        http_url: %s
-        patch_url: %s
-        user: %s <%s>
-        %s
-""" % (
-            pull['number'], pull['number'],
-            pull['html_url'], pull['patch_url'],
-            pull['user']['name'], pull['user']['email'],
-            pull['body']
-        ))
-    return pulls
+class GithubAPI(object):
+    def __init__(self):
+        pass
+    
+    def get_open_pull_requests(self):
+        open_reqs_url = API_BASE + "pulls/django/django/open"
+        log.debug('fetching: %s' % open_reqs_url)
+        r = requests.get(open_reqs_url)
+        log.debug(r.status_code)
+        if not r.status_code == 200:
+            log.error('Github returned a non-200 response!: %s' % r.content)
+    #    log.debug(r.content)
+        data = json.loads(r.content)
+        pulls = data['pulls']
+        return pulls
